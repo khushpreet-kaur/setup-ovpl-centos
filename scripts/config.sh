4e68e67d3c45eed7828ca5061fade3798f9cd08d
khushpreet-kaur <khushpreet@vlabs.ac.in>
2015-07-07 12:22:34 +0530

    add

---
+++ scripts/config.sh
@@ -0,0 +1,10 @@
+# set your http proxy based on your network's configuration.
+# Ask your network administrator if you are unsure about the
+# value
+
+# E.g.,
+# Note the port number after the second (:). 
+export http_proxy=""
+
+#set your https proxy.  Similar to the above.
+export https_proxy=""
